3ea30f5762d6005f94f8eb5ff0d807f4c8e38ed7
GBaykov <gleb.bykov99@gmail.com>
2022-03-07 14:48:31 +0300

    feat: creatr client

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
+import "./bootstrap.min.css";
+import { IPeople, IPlanet, IStarship } from "swapi-ts";
+//import { PersoneType } from "./types";
+//import { SwapiService } from "./swapi-service";
+//https://swapi.dev/api/
+
+console.log('Hello worls')
+
+ class SwapiService {
+    _apiBase = `https://swapi.dev/api`
+
+    async getResource(url:string=''){
+        const res = await fetch(`${this._apiBase}${url}`);
+        if(!res.ok) {
+            throw new Error (`Could not fetch ${this._apiBase}${url}` +
+            `, receivd ${res.status}`)
+        }
+        return await res.json()
+    }
+
+    async getAllPeople(){
+        const res = await this.getResource(`/people/`);
+        const people:IPeople[] = res.results;
+        return people;
+    }
+
+    async getPersone(id:string){
+        const persone:IPeople = await this.getResource(`/people/${id}/`)
+        return persone;
+    }
+
+    async getAllPlanets(){
+        const res = await this.getResource(`/planets/`);
+        const panets:IPlanet[] = res.results;
+        return panets;
+    }
+
+    async getPlanet(id:string){
+        const panet:IPlanet = await this.getResource(`/planets/${id}/`)
+        return panet;
+    }
+
+    async getAllStarships(){
+        const res = await this.getResource(`/starships/`);
+        const starships:IStarship[] = res.results;
+        return starships;
+    }
+
+    async getStarship(id:string){
+        const starship:IStarship = await this.getResource(`/starships/${id}/`);
+        return starship;
+    }
+}
+
+const api = new SwapiService()
+api.getStarship('3').then((people)=>{
+console.log(people.name)
+})
---
+++ src/types.ts
@@ -0,0 +1,24 @@
+// export type PersoneType =  {
+//     name: string, 
+//     height: string, 
+//     mass: string, 
+//     hair_color: string, 
+//     skin_color: string, 
+//     eye_color: string, 
+//     birth_year: string, 
+//     gender: string, 
+//     homeworld: string, 
+//     films: PersonsArrType[], 
+//     species: [], 
+//     vehicles: PersonsArrType[], 
+//     starships: PersonsArrType[], 
+//     created:string, 
+//     edited: string, 
+//     url: string,
+    
+// }
+// export type PersonsArrType = {
+//     prop:string
+// }
+
+
